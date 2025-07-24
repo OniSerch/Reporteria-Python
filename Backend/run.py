a389befed2251691a3c3158f293339227f08e6d3
OniSerch <Oniz641s@gmail.com>
2025-07-23 22:47:04 -0600

    frontend creado

--- Backend/run.py
+++ Backend/run.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from Backend.Routes import usuarios
 
-app = FastAPI()
+app = FastAPI(
+    title="API de Usuarios",
+    description="API para gestionar usuarios",
+)
 # Incluimos las rutas de usuarios en la aplicación FastAPI
 app.include_router(usuarios.usuarios)
